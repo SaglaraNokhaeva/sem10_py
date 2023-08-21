494b8c24896e32786ebfab6050af46345b271a7c
Saglara <nohaevass@yandex.ru>
2023-08-21 22:02:09 +0300

    1

--- dz1.py
+++ dz1.py
@@ -1,6 +1,5 @@
 # Доработаем задачи 5-6. Создайте класс-фабрику.
-# ○ Класс принимает тип животного (название одного из созданных классов)
-# и параметры для этого типа.
+# ○ Класс принимает тип животного (название одного из созданных классов) и параметры для этого типа.
 # ○ Внутри класса создайте экземпляр на основе переданного типа и
 # верните его из класса-фабрики.
 # Возьмите 1-3 любые задачи из прошлых семинаров (например сериализация
@@ -8,3 +7,62 @@
 # параметры в свойства. Задачи должны решаться через вызов методов
 # экземпляра.
 
+class Animal:
+    def __init__(self, name, age, voice = 'groal'):
+        self.name = name
+        self.age = age
+        self.voice = voice
+
+    def make_voice(self):
+        print(self.voice)
+
+class Fish(Animal):
+    def __init__(self, name, age, scales, voice):
+        super().__init__(name, age, voice)
+        self.scales = scales
+
+
+    def swim(self):
+        print("i'm swimming, oh, it's titan!")
+
+class Dog(Animal):
+    def __init__(self, name, age, breed, voice):
+        super().__init__(name, age, voice)
+        self.breed = breed
+
+    def bark(self):
+        print('Bark!')
+
+class Raven(Animal):
+    def __init__(self, name, age, color, voice):
+        super().__init__(name, age)
+        self.voice = voice
+        self.color = name
+
+    def fly_around_corpse(self):
+        print('oooh, meat....')
+
+
+class MakeAnimal():
+    def __init__(self, animal_type, age, voice='groal'):
+        self.name = name
+        self.age = age
+        self.voice = voice
+
+
+
+
+
+
+# fish = Fish('Nemo', 2, 'silver', 'bul-bul')
+# dog = Dog('Spark', 5, 'pitbull', 'bark!')
+# bird = Raven('Qarasique', 6, 'white', 'bravo!')
+#
+# animals = [fish, dog, bird]
+#
+# for i in animals:
+#     i.make_voice()
+#
+# fish.swim()
+# dog.bark()
+# bird.fly_around_corpse()
