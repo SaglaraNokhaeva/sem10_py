6e0fccc2da7c128c0d4833ef08bb77ff0ae12552
Saglara <nohaevass@yandex.ru>
2023-08-21 20:49:24 +0300

    1

--- ex5.py
+++ ex5.py
@@ -6,3 +6,35 @@
 # Для каждого класса создайте метод, выводящий
 # информацию специфичную для данного класса.
 
+class Fish():
+    def __init__(self, name, age, scales, voice):
+        super().__init__(name, age, voice)
+        self.scales = scales
+
+
+    def swim(self):
+        print("i'm swimming, oh, it's titan!")
+
+
+class Dog():
+    def __init__(self, name, age, breed, voice):
+        super().__init__(name, age, voice)
+        self.breed = breed
+
+    def bark(self):
+        print('Bark!')
+
+
+class Raven():
+    def __init__(self, name, age, color, voice):
+        super().__init__(name, age)
+        self.voice = voice
+        self.color = name
+
+    def fly_around_corpse(self):
+        print('oooh, meat....')
+
+fish = Fish('Nemo', 2, 'silver', 'bul-bul')
+dog = Dog('Spark', 5, 'pitbull', 'bark!')
+bird = Raven('Qarasique', 6, 'white', 'bravo!')
+
--- ex6.py
+++ ex6.py
@@ -5,3 +5,54 @@
 # Остальные классы наследуйте от него.
 # Убедитесь, что в созданные ранее классы внесены правки.
 
+class Animal:
+    def __init__(self, name, age, voice = 'groal'):
+        self.name = name
+        self.age = age
+        self.voice = voice
+
+    def make_voice(self):
+        print(self.voice)
+
+
+
+class Fish(Animal):
+    def __init__(self, name, age, scales, voice):
+        super().__init__(name, age, voice)
+        self.scales = scales
+
+
+    def swim(self):
+        print("i'm swimming, oh, it's titan!")
+
+
+class Dog(Animal):
+    def __init__(self, name, age, breed, voice):
+        super().__init__(name, age, voice)
+        self.breed = breed
+
+    def bark(self):
+        print('Bark!')
+
+
+class Raven(Animal):
+    def __init__(self, name, age, color, voice):
+        super().__init__(name, age)
+        self.voice = voice
+        self.color = name
+
+    def fly_around_corpse(self):
+        print('oooh, meat....')
+
+fish = Fish('Nemo', 2, 'silver', 'bul-bul')
+dog = Dog('Spark', 5, 'pitbull', 'bark!')
+bird = Raven('Qarasique', 6, 'white', 'bravo!')
+
+animals = [fish, dog, bird]
+
+for i in animals:
+    i.make_voice()
+
+fish.swim()
+dog.bark()
+bird.fly_around_corpse()
