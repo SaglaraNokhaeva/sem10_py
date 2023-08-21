20a4b6ac3a4d8187e5479f8e10bcb770c18538d8
Saglara <nohaevass@yandex.ru>
2023-08-21 18:03:28 +0300

    1

--- ex4.py
+++ ex4.py
@@ -6,3 +6,38 @@
 # ○ уровень доступа вычисляемый как остаток от деления
 # суммы цифр id на семь
 
+class Person:
+
+    def __init__(self, firstname, lastname, sex, age):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.sex = sex
+        self.__age = age
+
+    def birthday(self):
+        self.__age += 1
+
+    def full_name(self):
+      return f'{self.firstname} {self.lastname} {self.sex}'
+
+    def get_age(self):
+        return self.__age
+
+
+
+class Employee(Person):
+
+    def __init__(self, firstname, lastname, sex, age, pers_id):
+        if len(pers_id) != 6:
+            raise ValueError('Некорректный id!')
+        super().__init__(firstname, lastname, sex, age)
+        self.pers_id = pers_id
+        self.lvl_id = int(pers_id) % 7
+
+
+    def __str__(self):
+        return f'{self.firstname}: уровень: {self.lvl_id}, ID: {self.pers_id}'
+
+
+e1 = Employee('Вася', 'Иванов', 'М', 30, '123456')
+print(e1)
